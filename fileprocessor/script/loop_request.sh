ef67159523bacdaaa910dddb38f5cf27dcf8dd97
Kaniska_Sengupta <Kaniska_Sengupta@BLRKEC263587L.ad.infosys.com>
2024-09-20 11:12:57 +0530

    removed secret

--- fileprocessor/script/loop_request.sh
+++ fileprocessor/script/loop_request.sh
@@ -4,7 +4,7 @@ URL="https://api.github.com/zen"
 
 while true; do
         response=$(curl --request GET --url "$URL"  \
-                --header "Authorization: Bearer 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" \
+                --header "Authorization: Bearer $PUST" \
                   --header "X-GitHub-Api-Version: 2022-11-28")
         echo "$response"
         sleep 2
